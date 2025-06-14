eab35c311fc7ad5c8f948eb14e70bee67994e270
Raviravirego76466 <raviravirego@gmail.com>
2025-06-14 15:32:54 -0300

    Create bundle-lib.js

---
+++ scripts/bundle-lib.js
@@ -0,0 +1,4 @@
+var mod = Vars.mods.locateMod("exoprosopa");
+var change = "mod."+ modName + ".";
+mod.meta.displayName = Core.bundle.get(change+"name");
+mod.meta.description = Core.bundle.get(change+"description");
